d59246a5dc61fcae441d166b928e474141c29417
Anwesh43 <anweshthecool0@gmail.com>
2021-02-28 13:48:06 +0530

    adding LIBNode class

--- index.ts
+++ index.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class LIBNode {
+
+    prev : LIBNode 
+    next : LIBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LIBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLIBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LIBNode {
+        var curr : LIBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
