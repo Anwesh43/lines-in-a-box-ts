11e2e56195a7951f8561a7998d4effa638834b8f
Anwesh43 <anweshthecool0@gmail.com>
2021-02-28 11:31:08 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,12 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight
+const lines : number = 4 
+const parts : number = 5
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 3.4 
+const delay : number = 20
+const lineSizeFactor : number = 0.8 
+const boxYFactor : number = 0.2 
+
+
